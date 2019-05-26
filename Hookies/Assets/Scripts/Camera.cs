95c0e0c59667e0302f4dfb096b09481f81d5390a
MalshanPerera <33059694+MalshanPerera@users.noreply.github.com>
2019-05-27 00:21:55 +0530

    Movement

---
+++ Hookies/Assets/Scripts/Camera.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+
+{
+  
+
+    //public float speedH = 2.0f;
+    //public float speedV = 2.0f;
+
+    //private float yaw = 0.0f;
+    //private float pitch = 0.0f;
+
+    [SerializeField]
+    public Transform target;
+    private Vector3 offset;
+
+    [SerializeField]
+    private float rotateSpeed = 10f;
+
+    [SerializeField]
+    private Transform pivot;
+
+    void Start()
+    {
+        offset = target.position - transform.position;
+
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+    }
+
+    void Update()
+    {
+
+        //Get the X position of the mouse and rotate the player
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.Rotate(-vertical, 0, 0);
+
+        //Move the camera based on the current rotation of the player
+        float yAngle = target.eulerAngles.y;
+        float xAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(xAngle, yAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        //transform.position = target.position - offset;
+
+        transform.LookAt(target);
+
+        //yaw += speedH * Input.GetAxis("Mouse X");
+        //pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        //transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+
+}
--- Hookies/Assets/Scripts/PlayerController.cs
+++ Hookies/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody rb;
+
+    [SerializeField]
+    private float Speed = 500f;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, Speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -Speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(Speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-Speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(0, Speed * Time.deltaTime, 0);
+        }
     }
 }
