fd2847dea15e4bd3c4c6cb32c0bacde25e236f0c
therealsujji <sujithacrysis@gmail.com>
2019-05-26 23:21:41 +0530

    Hook done
    hook done movement needs to be completed

---
+++ Hookies/Assets/Scripts/SandBoxCharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandBoxCharacterController : MonoBehaviour
+{
+
+    new Camera camera;
+    private Vector3 moveDirection = Vector3.zero;
+    public float speed;
+    CharacterController controller;
+    void Start()
+    {
+        Cursor.visible = false;
+        Screen.lockCursor = true;
+        
+        camera = GameObject.FindObjectOfType<Camera>();
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    void FixedUpdate()
+    {
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+
+        // var characterRotation = camera.transform.rotation;
+        // characterRotation.x = 0;
+        // characterRotation.z = 0;
+
+        // transform.rotation = characterRotation;
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+}
